f8827bfc0584f712760228fd5c70c2cf5f8a061e
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2024-12-08 21:43:30 +0700

    Khởi tạo nhánh mới trắng

--- src/Network/play2.java
+++
@@ -1,9 +0,0 @@
-//package Network;
-//
-//public class play2 {
-//    public static Client2 pl1 = new Client2();
-//
-//    public static void main(String[] args) {
-//        pl1.P();
-//    }
-//}
